e43ae3ddbcd09e75865219b3ad3434b6e0ada899
Evan Connors <ecbconnors@gmail.com>
2018-07-18 16:57:30 -0400

    added mongoose to dependencies, hooked up controller to server.js

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
+const rabbitController = require('./controllers/rabbits.js');
+app.use('/rabbits', rabbitController);
+
 app.get('/', (req, res) => {
   res.send('Hello World')
 })
