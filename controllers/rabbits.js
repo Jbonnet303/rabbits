65c6e478cd8ebb702123c71fb4ad10fb1893d435
Evan Connors <ecbconnors@gmail.com>
2018-07-18 16:55:04 -0400

    model and controller

---
+++ controllers/rabbits.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const router = express.Router();
+const Rabbit = require('../models/rabbits.js');
+
+router.get('/', (req, res) => {
+  Rabbit.find({}, (err, rabbits) => {
+    if (err) {
+      console.log(err);
+      res.status(500).json(err);
+    } else {
+      res.status(200).json(rabbits);
+    }
+  })
+})
+
+router.post('/', (req, res) => {
+  Rabbit.create(req.body, (err, rabbit) => {
+    if (err) {
+      console.log(err);
+      res.status(500).json(err);
+    } else {
+      res.status(200).json(rabbit);
+    }
+  })
+})
+
+router.delete('/:id', (req, res) => {
+  Rabbit.findByIdAndRemove(req.params.id, (err, rabbit) => {
+    if (err) {
+      console.log(err);
+      res.status(500).json(err);
+    } else {
+      res.status(200).json(rabbit);
+    }
+  })
+})
+
+router.put('/:id', (req, res) => {
+  Rabbit.findByIdAndUpdate(req.params.id, (err, rabbit) => {
+    if (err) {
+      console.log(err);
+      res.status(500).json(err);
+    } else {
+      res.status(200).json(rabbit);
+    }
+  })
+})
